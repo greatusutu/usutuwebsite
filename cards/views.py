98de70e0d9b48060a3708350e1a83d4284120203
[Ntanganedzeni <[ntanganedzeni.sigidi@usutu.co.za]>
2024-02-20 15:32:36 +0200

    Team carousel first changes

--- cards/views.py
+++ cards/views.py
@@ -1,3 +1,11 @@
+from urllib import request
 from django.shortcuts import render
 
+from cards.admin import Carousel
+
 # Create your views here.
+projects = Carousel.objects.all()
+
+
+HttpResponse = render(request, 'template.html', {'projects': projects})
+
