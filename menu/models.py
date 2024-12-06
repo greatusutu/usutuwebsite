188931a826e4c4ac8ed84bf542772317b9ad0277
shaunmathebula <shaunmathebula02@gmail.com>
2024-12-06 15:42:08 +0200

    clients reviews button

--- menu/models.py
+++ menu/models.py
@@ -6,12 +6,13 @@ BUTTON_CHOICES = (
   ('team', 'Team Section'),
   ('contact', 'Contact Section'),
   ('partners', 'partners section'),
+  ('clients', 'clients section')
 )
 
 
 class Menu(models.Model):
   title = models.CharField(max_length=20)
-  external_link = models.CharField(max_length=20,help_text='For external links in menubar https://',blank=True)
+  external_link = models.CharField(max_length=40,help_text='For external links in menubar https://',blank=True)
   internal_link = models.CharField(choices=BUTTON_CHOICES,max_length=200,help_text='This will take you to a section of the page.Note that you should either give one of the link and not the both in same menu option',blank=True)
   is_published = models.BooleanField(default=True)
 
